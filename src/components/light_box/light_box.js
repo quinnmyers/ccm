ff0ea9191976468a908c9259651d9a86019bb3b0
Cole Smith <coles@Coles-MacBook-Pro.local>
2019-07-14 16:11:40 -0700

    re did the proj page

--- src/components/light_box/light_box.js
+++ src/components/light_box/light_box.js
@@ -3,7 +3,8 @@ import Lightbox from "lightbox-react"
 import "lightbox-react/style.css"
 import Img from "gatsby-image"
 import "./light_box.sass"
-
+import { BLOCKS, MARKS } from "@contentful/rich-text-types"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 class LightBox extends Component {
   constructor(props) {
     super(props)
@@ -14,31 +15,37 @@ class LightBox extends Component {
   }
   render() {
     const { photoIndex, isOpen } = this.state
+    const Text = ({ children }) => <p>{children}</p>
+    const options = {
+      renderNode: {
+        [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+      },
+      renderText: text => {
+        return text.split("\n").reduce((children, textSegment, index) => {
+          return [...children, index > 0 && <br key={index} />, textSegment]
+        }, [])
+      },
+    }
     return (
-      <div className="lightbox">
-        {this.props.photos.map((img, index) => (
-          <div
-            className="lightbox__single"
-            onClick={() => this.setState({ isOpen: true, photoIndex: index })}
-            key={index}
-          >
-            <h4>{img.node.title}</h4>
-            <p>{img.node.city}</p>
-            <Img fluid={img.node.image.fluid} alt={img.node.title} />
-          </div>
-        ))}
+      <div className="lightbox" onClick={() => this.setState({ isOpen: true })}>
+        <Img fluid={this.props.cover} alt={this.props.alt} />
+
         {isOpen && (
           <Lightbox
-            mainSrc={this.props.photos[photoIndex].node.image.file.url}
+            imageCaption={documentToReactComponents(
+              this.props.caption,
+              options
+            )}
+            mainSrc={this.props.photos[photoIndex].file.url}
             nextSrc={
               this.props.photos[(photoIndex + 1) % this.props.photos.length]
-                .node.image.file.url
+                .file.url
             }
             prevSrc={
               this.props.photos[
                 (photoIndex + this.props.photos.length - 1) %
                   this.props.photos.length
-              ].node.image.file.url
+              ].file.url
             }
             onCloseRequest={() => this.setState({ isOpen: false })}
             onMovePrevRequest={() =>
