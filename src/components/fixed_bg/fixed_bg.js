b4721cdd746d65692c57b29477cc28a7fac30ed5
Cole Smith <coles@Coles-MacBook-Pro.local>
2019-07-19 22:24:42 -0700

    fixed broken home page images

--- src/components/fixed_bg/fixed_bg.js
+++ src/components/fixed_bg/fixed_bg.js
@@ -3,7 +3,27 @@ import BackgroundImage from "gatsby-background-image"
 class FixedBg extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      mob: false,
+    }
+  }
+  componentDidMount() {
+    if (window.matchMedia("(max-width: 600px)").matches) {
+      this.setState({ mob: true })
+    }
+  }
+  styles = () => {
+    if (this.state.mob) {
+      return {
+        backgroundPosition: "0px 0px",
+        backgroundSize: "cover",
+      }
+    } else {
+      return {
+        backgroundAttachment: "fixed",
+        backgroundSize: "cover",
+      }
+    }
   }
   render() {
     return (
@@ -12,7 +32,7 @@ class FixedBg extends Component {
         className="fixed"
         fluid={this.props.backGroundImage}
         backgroundColor={`#98816e`}
-        style={{ backgroundAttachment: "fixed" }}
+        style={this.styles()}
         fadeIn={true}
       >
         {this.props.children}
