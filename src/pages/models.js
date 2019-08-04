3da1fef9920ea6afb2dac23c7841046ed4e29e6d
Cole Smith <coles@Coles-MacBook-Pro.local>
2019-08-04 14:30:32 -0700

    trying something

---
+++ src/pages/models.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react"
+import "../components/pagestyles/projects.sass"
+import Layout from "../components/layout/layout"
+import SEO from "../components/seo"
+import Content from "../components/utility/Content/Content"
+
+class Models extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  componentDidMount() {
+    console.log(this.props.data)
+  }
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <Content>
+          <div className="page">
+            <a href="pano.html">try?</a>
+            <a href="alsoMove/pano.html">try?</a>
+          </div>
+        </Content>
+      </Layout>
+    )
+  }
+}
+export default Models
