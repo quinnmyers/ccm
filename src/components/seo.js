1679f0c46a8136adf1d9a8c3e42ffe3913ab3297
Quinn Myers <quinnmyers@Quinns-MacBook-Pro-2.local>
2019-09-28 14:35:13 -0700

    added og image

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,64 +3,77 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { StaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, keywords, title }) {
+import ogImage from "./ccm_og_image.jpg"
+
+function SEO({ description, lang, meta, keywords, title, page, image }) {
   return (
     <StaticQuery
       query={detailsQuery}
       render={data => {
         const metaDescription =
           description || data.site.siteMetadata.description
+        const metaImage = ogImage || image
         return (
-          <Helmet
-            htmlAttributes={{
-              lang,
-            }}
-            title={title}
-            titleTemplate={`%s | ${data.site.siteMetadata.title}`}
-            meta={[
-              {
-                name: `description`,
-                content: metaDescription,
-              },
-              {
-                property: `og:title`,
-                content: title,
-              },
-              {
-                property: `og:description`,
-                content: metaDescription,
-              },
-              {
-                property: `og:type`,
-                content: `website`,
-              },
-              {
-                name: `twitter:card`,
-                content: `summary`,
-              },
-              {
-                name: `twitter:creator`,
-                content: data.site.siteMetadata.author,
-              },
-              {
-                name: `twitter:title`,
-                content: title,
-              },
-              {
-                name: `twitter:description`,
-                content: metaDescription,
-              },
-            ]
-              .concat(
-                keywords.length > 0
-                  ? {
-                      name: `keywords`,
-                      content: keywords.join(`, `),
-                    }
-                  : []
-              )
-              .concat(meta)}
-          />
+          <div>
+            <Helmet
+              htmlAttributes={{
+                lang,
+              }}
+              title={data.site.siteMetadata.title}
+              titleTemplate={`%s | ${data.site.siteMetadata.description}`}
+              meta={[
+                {
+                  name: `description`,
+                  content: metaDescription,
+                },
+                {
+                  property: `og:title`,
+                  content: `${data.site.siteMetadata.title} - ${page}`,
+                },
+                {
+                  property: `og:description`,
+                  content: metaDescription,
+                },
+                {
+                  property: `og:type`,
+                  content: `website`,
+                },
+                {
+                  property: `og:url`,
+                  content: `https://www.thepastryacademy.com`,
+                },
+                {
+                  property: `og:image`,
+                  content: metaImage,
+                },
+                {
+                  name: `twitter:card`,
+                  content: `summary`,
+                },
+                {
+                  name: `twitter:creator`,
+                  content: data.site.siteMetadata.author,
+                },
+                {
+                  name: `twitter:title`,
+                  content: data.site.siteMetadata.title,
+                },
+                {
+                  name: `twitter:description`,
+                  content: metaDescription,
+                },
+              ]
+                .concat(
+                  keywords.length > 0
+                    ? {
+                        name: `keywords`,
+                        content: keywords.join(`, `),
+                      }
+                    : []
+                )
+                .concat(meta)}
+            />
+          </div>
         )
       }}
     />
@@ -78,7 +91,7 @@ SEO.propTypes = {
   lang: PropTypes.string,
   meta: PropTypes.array,
   keywords: PropTypes.arrayOf(PropTypes.string),
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
 }
 
 export default SEO
